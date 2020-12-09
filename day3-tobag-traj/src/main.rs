e15c748010c3f82b22bc50a55af66d8164e54fbf
Manikandan Sundararajan <mani@hey.com>
2020-12-09 05:44:47 -0500

    Simplify part 1 solution

--- day3-tobag-traj/src/main.rs
+++ day3-tobag-traj/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, io};
+use std::fs;
 
 type Slope = (usize, usize);
 type Pos = Slope;
@@ -13,35 +13,26 @@ fn main() {
 
     // Convert the string representation of the input into a vector of strs
     let trees: Vec<_> = contents.split_ascii_whitespace().collect();
-    part_one(&trees);
+    let slope: Slope = (3, 1);
+    part_one(&trees, slope);
 }
 
-fn part_one(trees: &Vec<&str>) {
-    let slope: Slope = (3, 1); // Slope description ; 3 right 1 down
+fn part_one(trees: &Vec<&str>, slope: Slope) {
     let mut cur_pos: Pos = (0, 0); // Current position of tobaggons
     let size_of_treeline = trees.get(0).expect("Something went wrong").len();
-    // Count of tree encounters
-    let mut encounters: usize = match get_at_location(&trees, cur_pos.0, cur_pos.1).unwrap() {
-        '#' => 1,
-        _ => 0,
-    };
+    let mut encounters: usize = 0;
 
-    for _ in 1..trees.len() {
-        let (x, y): Pos = (cur_pos.0 + slope.0, cur_pos.1 + slope.1);
-
-        if x <= size_of_treeline {
-            encounters += match get_at_location(&trees, x, y).unwrap() {
-                '#' => 1,
-                _ => 0,
-            };
-        } else {
-            let x = (cur_pos.0 + slope.0) % size_of_treeline; // Shadow x here
-            encounters += match get_at_location(&trees, x, y).unwrap() {
-                '#' => 1,
-                _ => 0,
-            };
+    for _ in 0..trees.len() {
+        let (mut x, y): Pos = cur_pos;
+        if y >= trees.len() {
+            break;
         }
-        cur_pos = (x, y);
+        x %= size_of_treeline;
+        encounters += match get_at_location(&trees, x, y).unwrap() {
+            '#' => 1,
+            _ => 0,
+        };
+        cur_pos = (x + slope.0, y + slope.1);
     }
 
     println!("Number of tree encounters: {}", encounters);
