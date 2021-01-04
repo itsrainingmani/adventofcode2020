d8f78d61b120b1d54d91b6167c1315da56363138
Manikandan Sundararajan <mani@hey.com>
2021-01-03 23:55:57 -0600

    Parse the input.txt file

--- day4-pass-proc/src/main.rs
+++ day4-pass-proc/src/main.rs
@@ -1,3 +1,20 @@
+use std::fs;
+
 fn main() {
     println!("Advent of Code 2020: Day 4: Passport Processing");
+
+    let input_filename = String::from("input.txt");
+
+    let contents =
+        fs::read_to_string(input_filename).expect("Something went wrong reading the file");
+
+    // Split each passport into a vector of key:value pairs
+    let passports_vec: Vec<Vec<&str>> = contents
+        .split("\n\n")
+        .collect::<Vec<&str>>()
+        .iter()
+        .map(|s| s.split_ascii_whitespace().collect())
+        .collect();
+
+    println!("{:#?}", passports_vec);
 }
