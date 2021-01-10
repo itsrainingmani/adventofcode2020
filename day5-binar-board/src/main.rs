cbc9b06ef21360a7068be0cdab9a703dbe8534ea
Manikandan Sundararajan <mani@hey.com>
2021-01-09 23:02:48 -0600

    Split input file into vector of str

--- day5-binar-board/src/main.rs
+++ day5-binar-board/src/main.rs
@@ -7,4 +7,9 @@ fn main() {
 
     let contents =
         fs::read_to_string(input_filename).expect("Something went wrong reading the file");
+
+    // Split each passport into a vector of key:value pairs
+    let boarding_vec: Vec<&str> = contents.split("\n").collect::<Vec<&str>>();
+
+    println!("{:#?}", boarding_vec);
 }
