b14da813189fdcbf838a771c7f04b3aa49f28984
Manikandan Sundararajan <mani@hey.com>
2021-01-09 23:02:23 -0600

    Read the input file

--- day5-binar-board/src/main.rs
+++ day5-binar-board/src/main.rs
@@ -1,3 +1,10 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Day 5: Binary Board");
+
+    let input_filename = String::from("input.txt");
+
+    let contents =
+        fs::read_to_string(input_filename).expect("Something went wrong reading the file");
 }
